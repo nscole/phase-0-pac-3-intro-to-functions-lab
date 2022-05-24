726ac456c91a9da7ec348002d781a1ab4eeeb9f4
Naomi Cole <naomisarahcole@gmail.com>
2022-05-24 17:35:24 +1000

    completed first 2 passes

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string)
+{
+  if (string)
+    return string.toUpperCase('Hello!');
+}
+
+function whisper(string)
+{
+    if (string)
+      return string.toLowerCase('Hello!');
+  }
+
+function
