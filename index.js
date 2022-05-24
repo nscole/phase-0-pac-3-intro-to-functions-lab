85e7c86ac2fdbc85b57f2e1bc4e0c37839e230b1
Naomi Cole <naomisarahcole@gmail.com>
2022-05-24 18:55:46 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function whisper(string)
       return string.toLowerCase('Hello!');
   }
 
-function
+  function logShout (string) {
+    console.log(string.toUpperCase('Hello!'));
+  }
+
+  function logWhisper (string) {
+    console.log(string.toLowerCase('Hello!'));
+  }
+
+
+function sayHiToGrandma(string) {
+    if (string.toLowerCase() === string)
+     return "I can\'t hear you!"
+        else if (string.toUpperCase() === string)
+      return "YES INDEED!"
+       else (string === "I love you, Grandma.")
+       return "I love you, too."
+  }
